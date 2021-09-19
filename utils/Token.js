4055f7d899675a6cf3ddf0878aa6f65274d2194b
Gydo Makkinga <gydo@makkinga.online>
2021-09-19 09:34:29 +0200

    Updated circulating supply calculation

--- utils/Token.js
+++ utils/Token.js
@@ -83,6 +83,12 @@ exports.volume = async function () {
  */
 exports.circulatingSupply = async function () {
     const graveyard = [
+        '0x28d9475f6354091a49e20a897f6405a02ffd6836',
+        '0x328983c8331a8ad6f08036f2983a8268f9e0f46d',
+        '0xfef8bd2e06d8117e51ce7b960992e4055997d9fe',
+        '0x194e7650fe17c2c478cd6d147620790c9e811c3f',
+        '0x038eb501cef9d37e1a418ba28f66bd535123a6e7',
+        '0x2b9f62ac65bcf956b6e15ec427456b2cf3a51992',
         '0x000000000000000000000000000000000000dead',
         '0x48a30b33ebd0afac1d8023e06e17372c21c0fb18',
         '0x9b68bf4bf89c115c721105eaf6bd5164afcc51e4',

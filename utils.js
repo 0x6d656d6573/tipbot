1ff8cb38a0d588e2bcd94e9cce528893e3e69856
Gydo Makkinga <gydo@makkinga.online>
2021-09-21 21:43:20 +0200

    Added Cat command

--- utils.js
+++ utils.js
@@ -1,5 +1,6 @@
 module.exports = {
     Config        : require('./utils/Config'),
+    Helpers       : require('./utils/Helpers'),
     Log           : require('./utils/Log'),
     DB            : require('./utils/DB'),
     React         : require('./utils/React'),
